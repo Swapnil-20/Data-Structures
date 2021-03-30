5980aa01be1683fc4e42c98fbf9544d79de1c738
Swapnil <schaturvedi.cse18@chitkarauniversity.edu.in>
2021-03-30 10:33:01 +0530

    Adding node in end of linked list

---
+++ AddLinkList.java
@@ -0,0 +1,61 @@
+import java.util.*;
+     class Node{
+         String data;
+     Node next;
+        public Node(String data)
+        {
+            this.data=data;
+        }
+
+        public String getData() {
+            return this.data;
+        }
+
+        public void setData(String data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return this.next;
+        }
+
+        public void setNext(Node next) {
+            this.next = next;
+        }
+
+    }
+
+class LinkedList{
+         Node head;
+         Node tail;
+
+        public Node getHead() {
+            return this.head;
+        }
+
+        public Node getTail() {
+            return this.tail;
+        }
+        public void addNode(String data) {
+            Node node=new Node(data);//create node
+            if(head==null)
+            {
+                head=tail=node;
+            }
+            else{
+                tail.next=node;
+                tail=node;
+            }
+        }
+        
+}
+class Tester{
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+		list.addNode("Milan");
+		list.addNode("Venice");
+		list.addNode("Munich");
+		list.addNode("Vienna");
+		System.out.println("Adding an element to the linked list ");
+	}
+    }
