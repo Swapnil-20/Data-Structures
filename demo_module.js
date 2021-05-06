3aedc3cad795eecf2825eeeb058356b209c7354c
Swapnil <schaturvedi.cse18@chitkarauniversity.edu.in>
2021-05-06 18:43:34 +0530

    Created my first module

---
+++ demo_module.js
@@ -0,0 +1,7 @@
+var http=require('http');
+var dt=require('./myfirstmodule');
+http.createServer(function(req,res){
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.write("Date and Time are "+dt.myDateTime());
+    res.end();
+}).listen(8081);
